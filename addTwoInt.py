534d54a650f9cbf0399ab9501a73624d3992bfb9
zahrah925 <zahrahkaliapermal@gmail.com>
2020-03-10 16:15:59 +0400

    qu9 done

--- addTwoInt.py
+++ addTwoInt.py
@@ -5,13 +5,7 @@ import sys
 def add(a, b):
     a = int( sys.argv[1] )
     b = int( sys.argv[2] )
-    return 0
 
 if __name__=="__main__":
-    if (len(sys.argv) > 3):
-        print ("Erreur, entrez que 2 variables, svp!")
-    elif (len(sys.argv) < 3):
-        print("Erreur, entrez que 2 variables, svp!")
-    else:
-        print( sys.argv)
+    print(sys.argv)
 
