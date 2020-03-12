ccea1de9083f1176cfe78474f0fd5576dbb58d56
zahrah925 <zahrahkaliapermal@gmail.com>
2020-03-12 15:00:54 +0400

    tag 0.3 qu13 done

--- addTwoInt.py
+++ addTwoInt.py
@@ -3,14 +3,29 @@
 import sys
 
 def add(a, b):
-    a = int( sys.argv[1] )
-    b = int( sys.argv[2] )
-    return 0
+   # a = int(sys.argv[1])
+   # b = int(sys.argv[2])
+    return a,b
 
 if __name__=="__main__":
+
     if (len(sys.argv) > 3):
         print ("Erreur, entrez que 2 variables, svp!")
-    elif (len(sys.argv) < 3):
-        print("Erreur, entrez que 2 variables, svp!")
+
+    elif (len(sys.argv) == 1):
+        print("Erreur, entrez deux variables, svp!")
+        a = int(input("Premier valeur : "))
+        b = int(input("Deuxieme valeur : "))
+        print( add(a, b) )
+
+    elif (len(sys.argv) == 2):
+        print("Erreur, entrez encore une valeur, svp!")
+        b = input("Deuxieme valeur : ")
+        a = int( sys.argv[1] )
+        b = int(b)
+        print( add(a, b))
+
     else:
-        print( sys.argv)
+        a = int( sys.argv[1] )
+        b = int( sys.argv[2] )
+        print( add(a, b) )
